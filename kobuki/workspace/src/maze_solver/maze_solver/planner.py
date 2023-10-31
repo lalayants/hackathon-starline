a04700eb2e98f394a99a23c312db434ecbb2238f
tb9 <antipov.vl.a@yandex.ru>
2023-10-31 17:58:19 +0300

    with auto graph movement

--- kobuki/workspace/src/maze_solver/maze_solver/planner.py
+++ kobuki/workspace/src/maze_solver/maze_solver/planner.py
@@ -13,17 +13,21 @@ from tf2_ros.transform_listener import TransformListener
 from tf2_ros import TransformException 
 from std_msgs.msg import Empty
 from example_interfaces.srv import SetBool
+from nav_msgs.msg import Path
+
 import time
 
 class Grid():
 
-    def __init__(self, n_width:int = 6, n_length:int = 5, cell_size:float = 1.0):
+    def __init__(self, n_width:int = 3, n_length:int = 3, cell_size:float = 1.0):
         self.origin_pose = [0.0, 0.0]
-        self.zero_cell_pose = (0.8, 2.2)
+        self.zero_cell_pose = (0.8, 1.2)
         self.n_width = n_width
         self.n_length = n_length
         self.cell_size = cell_size
         self.grid = [self.zero_cell_pose]
+        self.used = [0]
+        self.pred = [-1]
 
         self.create_grid()
 
@@ -35,6 +39,8 @@ class Grid():
                 y = -j*self.cell_size + self.zero_cell_pose[1]
                 x = i*self.cell_size + self.zero_cell_pose[0]
                 self.grid.append((x, y))
+                self.used.append(0)
+                self.pred.append(-1)
 
     def get_nearest_cell(self, robot_pose):
         pass        
@@ -48,8 +54,10 @@ class GoalPublisher(Node):
         super().__init__('goal_publisher')
         self.publisher_ = self.create_publisher(PoseStamped, '/goal_pose', 10)
         timer_period = 0.5  # seconds
-        self.path = (2, 1, 0, 6, 12, 7, 6, 0, 1, 2, 3, 4, 5, 11, 10, 9, 15, 16, 17, 22, 28, 29, 27, 26, 25, 26, 27, 28, 22, 16, 15, 21, 20, 14, 8, 2)
-        self.goal = 0
+        # self.path = (2, 1, 0, 6, 12, 7, 6, 0, 1, 2, 3, 4, 5, 11, 10, 9, 15, 16, 17, 22, 28, 29, 27, 26, 25, 26, 27, 28, 22, 16, 15, 21, 20, 14, 8, 2)
+        self.path = (2, 1)
+        self.current = -1
+        self.goal = 1
         self.grid = Grid()
         self.is_reached = False
         self.time_stamp = Clock().now()
@@ -62,11 +70,24 @@ class GoalPublisher(Node):
             '/aruco_found',
             self.aruco_callback,
             1)
+        
+        self.path_subscription = self.create_subscription(
+            Path,
+            '/transformed_global_plan',
+            self.path_callback,
+            1)
         self.n_found = 0
         self.last_found = time.time()
         
         self.cli = self.create_client(SetBool, "toggle_stabilization")
     
+    def path_callback(self, msg):
+        path_length = 0
+        for i in range(1, len(msg.poses)):
+            path_length += ((msg.poses[i].pose.position.x - msg.poses[i - 1].pose.position.x)**2 + (msg.poses[i].pose.position.y - msg.poses[i - 1].pose.position.y)**2)**0.5
+
+        #print("PATH LENGTH:", path_length)
+
     def aruco_callback(self, msg): 
         print(time.time(),self.last_found)
         if time.time() - self.last_found > 1:
@@ -103,13 +124,39 @@ class GoalPublisher(Node):
         if pose is None:
             return 
         
-        goal_pose = self.grid.grid[self.path[self.goal]]
+        goal_pose = self.grid.grid[self.goal]
         error = ((pose[0] - goal_pose[0])**2 + (pose[1] - goal_pose[1])**2)**(0.5) 
-        print(pose, goal_pose, error)
+        
         if error < 0.4:
-            if self.goal < len(self.path) - 1: 
-                self.goal +=1
-
+            print('Reached goal: ', self.goal)
+            self.grid.pred[self.goal] = self.current
+            self.current = self.goal
+            self.grid.used[self.current] = 1
+            flag = True
+            print(self.goal % self.grid.n_width)
+            if self.goal % self.grid.n_width > 0:
+                if self.grid.used[self.goal - 1] == 0:
+                    # Has left neigbour
+                    self.goal -= 1
+                    flag = False
+            if (self.goal < self.grid.n_length * (self.grid.n_width - 1) - 1):
+                if self.grid.used[self.goal + self.grid.n_width] == 0 and flag:
+                    # Has up neigbour
+                    self.goal += self.grid.n_width  
+                    flag = False
+            if self.goal % self.grid.n_width < self.grid.n_width - 1:
+                if self.grid.used[self.goal + 1] == 0 and flag:
+                    # Has right neigbour
+                    self.goal += 1
+                    flag = False
+            if self.goal >= self.grid.n_width:
+                if self.grid.used[self.goal - self.grid.n_width] == 0 and flag:
+                    # Has down neigbour
+                    self.goal -= self.grid.n_width
+                    flag = False
+            if flag:
+                self.goal = self.grid.pred[self.current]
+            print('Next goal: ', self.goal)
         pose = PoseStamped()
 
         pose.header.frame_id = 'map'
